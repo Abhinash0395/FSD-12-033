396e852d97ce8cf4cd660154a0871d794bf9abc7
Abhinash Rai <rainikhil3112@gmail.com>
2026-07-24 14:58:45 +0530

    fun.js

---
+++ LAB 1/fun.js
@@ -0,0 +1,25 @@
+const toWords = (digit)=> {
+    const words = [
+      "Zero",
+      "One",
+      "Two",
+      "Three",
+      "Four",
+      "Five",
+      "Six",
+      "Seven",
+      "Eight",
+      "Nine",
+    ];
+    return words[digit];
+}
+console.log(toWords(8));
+
+
+// react frontend
+// VEW.js
+// FLASK
+// Angular
+// Bootstrap
+// Tailwind
+// Node.js, Springboot, fast api, django, PHP  these are backend 
